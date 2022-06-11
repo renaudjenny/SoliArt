2c253b3fb7896de95eaa4fb0518dfcb7ef28ecbf
Renaud Jenny <renaud.jenny@gmail.com>
2022-06-11 08:36:37 +0100

    Initial Commit

---
+++ SoliArt/SoliArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoliArtApp.swift
+//  SoliArt
+//
+//  Created by Renaud JENNY on 11/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SoliArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
