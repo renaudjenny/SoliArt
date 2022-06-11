70f7384515fa7afa178743d36216e68ba9106168
Renaud Jenny <renaud.jenny@gmail.com>
2022-06-11 08:40:45 +0100

    refactor: remove boilerplate code

---
+++ SoliArt/AppView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
--- SoliArt/SoliArtApp.swift
+++ SoliArt/SoliArtApp.swift
@@ -1,17 +1,10 @@
-//
-//  SoliArtApp.swift
-//  SoliArt
-//
-//  Created by Renaud JENNY on 11/06/2022.
-//
-
 import SwiftUI
 
 @main
 struct SoliArtApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppView()
         }
     }
 }
