b9d804acf5087964ac2bbb1852040ee9b387b8df
Renaud Jenny <renaud.jenny@gmail.com>
2022-06-14 21:51:28 +0100

    feat: improve the board view

--- SoliArt/AppView.swift
+++ SoliArt/AppView.swift
@@ -1,3 +1,4 @@
+import ComposableArchitecture
 import SwiftUI
 import SwiftUICardGame
 
@@ -5,28 +6,56 @@ struct AppView: View {
     var body: some View {
         VStack {
             HStack {
-                CardVerticalDeckView(
-                    cards:
-                        Array(repeating: StandardDeckCard(.ace, of: .spades, isFacedUp: false, action: {}), count: 2)
-                        + [StandardDeckCard(.ace, of: .spades, isFacedUp: true, action: {})],
-                    cardHeight: 100,
-                    facedDownSpacing: 10,
-                    facedUpSpacing: 20
-                )
-                Spacer()
                 HStack {
-                    ForEach(StandardDeckCard.Suit.allCases) { suit in
-                        suit.view.fill(style: .init(eoFill: true, antialiased: true))
+                    ForEach(suits) { suit in
+                        RoundedRectangle(cornerRadius: 4)
+                            .stroke()
+                            .frame(width: 50, height: 70)
+                            .overlay(
+                                suit.view
+                                    .fill(style: .init(eoFill: true, antialiased: true))
+                                    .foregroundColor(suit.color)
+                                    .padding()
+                        )
                     }
                 }
+                .frame(maxHeight: .infinity, alignment: .top)
+
+                HStack {
+                    CardVerticalDeckView(
+                        cards: Array(
+                            [StandardDeckCard].standard52Deck(action: { _, _ in })
+                                .map {
+                                    var card = $0
+                                    card.isFacedUp = true
+                                    return card
+                                }
+                                .prefix(3)
+                        ),
+                        cardHeight: 70,
+                        facedDownSpacing: 3,
+                        facedUpSpacing: 2
+                    )
+                    CardVerticalDeckView(
+                        cards: Array([StandardDeckCard].standard52Deck(action: { _, _ in }).prefix(3)),
+                        cardHeight: 70,
+                        facedDownSpacing: 3,
+                        facedUpSpacing: 0
+                    )
+                }
+                .fixedSize(horizontal: true, vertical: false)
+                .frame(maxWidth: .infinity, alignment: .trailing)
             }
+            .frame(height: 150)
             HStack {
                 ForEach(decks, id: \.indices) {
-                    CardVerticalDeckView(cards: $0, cardHeight: 100, facedDownSpacing: 20, facedUpSpacing: 10)
+                    CardVerticalDeckView(cards: $0, cardHeight: 70, facedDownSpacing: 20, facedUpSpacing: 10)
                 }
             }
         }.padding()
     }
+
+    var suits: [StandardDeckCard.Suit] { [.hearts, .spades, .diamonds, .spades] }
 
     var cards: [StandardDeckCard] {
         .standard52Deck(action: { _, _ in })
