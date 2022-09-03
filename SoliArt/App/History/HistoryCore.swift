f7927cccd1fc23b527fa5ac33305c07a639339df
Renaud Jenny <renaud.jenny@gmail.com>
2022-09-03 22:08:08 +0100

    feat(History): add Undo logic

--- SoliArt/App/History/HistoryCore.swift
+++ SoliArt/App/History/HistoryCore.swift
@@ -6,16 +6,20 @@ struct HistoryState: Equatable {
 
 enum HistoryAction: Equatable {
     case addEntry(GameState)
+    case undo
 }
 
 struct HistoryEnvironment {
-    let now: () -> Date
+    var now: () -> Date
 }
 
 let historyReducer = Reducer<HistoryState, HistoryAction, HistoryEnvironment> { state, action, environment in
     switch action {
     case let .addEntry(gameState):
         state.entries.append(HistoryEntry(date: environment.now(), gameState: gameState))
+        return .none
+    case .undo:
+        state.entries = state.entries.dropLast()
         return .none
     }
 }
--- SoliArtTests/History/HistoryCoreTests.swift
+++ SoliArtTests/History/HistoryCoreTests.swift
@@ -5,20 +5,39 @@ import XCTest
 
 class HistoryCoreTests: XCTestCase {
     private var store: TestStore<HistoryState, HistoryState, HistoryAction, HistoryAction, HistoryEnvironment>!
-    var now: () -> Date = { Date(timeIntervalSince1970: 0) }
 
     @MainActor override func setUp() async throws {
         store = TestStore(
             initialState: HistoryState(),
             reducer: historyReducer,
-            environment: HistoryEnvironment(now: now)
+            environment: HistoryEnvironment(now: { Date(timeIntervalSince1970: 0) })
         )
     }
 
     func testAddEntry() {
         let state = GameState()
+        let now = store.environment.now()
         store.send(.addEntry(state)) {
-            $0.entries = [HistoryEntry(date: self.now(), gameState: state)]
+            $0.entries = [HistoryEntry(date: now, gameState: state)]
+        }
+    }
+
+    func testUndo() {
+        testAddEntry()
+
+        var state2 = GameState()
+        state2.foundations.append(Foundation(suit: .clubs, cards: [Card(.ace, of: .clubs, isFacedUp: true)]))
+        state2.piles.append(Pile(id: 1, cards: [Card(.two, of: .clubs, isFacedUp: true)]))
+
+        let date = Date(timeIntervalSince1970: 60)
+        store.environment.now = { date }
+
+        store.send(.addEntry(state2)) {
+            $0.entries += [HistoryEntry(date: date, gameState: state2)]
+        }
+
+        store.send(.undo) {
+            $0.entries = [self.store.state.entries.first!]
         }
     }
 }
