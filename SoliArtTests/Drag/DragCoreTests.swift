1898c36a0a6089b57560333525530e42d30673a5
Renaud Jenny <renaud.jenny@gmail.com>
2023-06-23 22:07:23 +0200

    feat(Drag): fix test for drop card to an other pile

--- SoliArtTests/Drag/DragCoreTests.swift
+++ SoliArtTests/Drag/DragCoreTests.swift
@@ -3,47 +3,55 @@ import ComposableArchitecture
 import SwiftUICardGame
 import XCTest
 
+@MainActor
 class DragCoreTests: XCTestCase {
-    private var store = TestStore(
-        initialState: Drag.State(
-            piles: GameCoreTests.pilesAfterShuffle(),
-            foundations: Game.State().foundations
-        )
-    ) {
-        Drag()
-    }
-
-    func testDragCards() {
+    func testDragCards() async {
+        let store = TestStore(
+            initialState: Drag.State(
+                piles: GameCoreTests.pilesAfterShuffle(),
+                foundations: Game.State().foundations
+            )
+        ) {
+            Drag()
+        }
         let scheduler = DispatchQueue.test
         store.dependencies.mainQueue = scheduler.eraseToAnyScheduler()
         let state = DraggingState(card: Card(.six, of: .clubs, isFacedUp: true), position: CGPoint(x: 123, y: 123))
-        store.send(.dragCard(state.card, position: state.position)) {
+        await store.send(.dragCard(state.card, position: state.position)) {
             $0.draggingState = state
             $0.zIndexPriority = .pile(id: 3)
         }
 
-        store.send(.dropCards) {
+        await store.send(.dropCards) {
             $0.draggingState = nil
         }
 
-        scheduler.advance(by: 0.5)
+        await scheduler.advance(by: 0.5)
 
-        store.receive(.resetZIndexPriority) {
+        await store.receive(.resetZIndexPriority) {
             $0.zIndexPriority = .pile(id: 1)
         }
     }
 
 
-    func testUpdateFrame() {
+    func testUpdateFrame() async {
+        let store = TestStore(
+            initialState: Drag.State(
+                piles: GameCoreTests.pilesAfterShuffle(),
+                foundations: Game.State().foundations
+            )
+        ) {
+            Drag()
+        }
         let frame: Frame = .pile(0, CGRect(x: 100, y: 100, width: 100, height: 200))
-        store.send(.updateFrames([frame])) {
+        await store.send(.updateFrames([frame])) {
             $0.frames = IdentifiedArrayOf(uniqueElements: [frame])
         }
     }
 
-    func testDropCardsToAnOtherPile() {
+    func testDropCardsToAnOtherPile() async {
         let scheduler = DispatchQueue.test
-        store = TestStore(
+        let store = TestStore(
             initialState: Drag.State(
                 piles: GameCoreTests.pilesAfterShuffleForEasyGame()
             )
@@ -53,44 +61,39 @@ class DragCoreTests: XCTestCase {
             $0.mainQueue = scheduler.eraseToAnyScheduler()
         }
 
-        let frame: Frame = .pile(5, CGRect(x: 100, y: 100, width: 100, height: 200))
-        store.send(.updateFrames([frame])) {
+        let frame: Frame = .pile(2, CGRect(x: 100, y: 100, width: 100, height: 200))
+        await store.send(.updateFrames([frame])) {
             $0.frames = IdentifiedArrayOf(uniqueElements: [frame])
         }
 
-        let dragCard = Card(.ace, of: .spades, isFacedUp: true)
+        let dragCard = Card(.seven, of: .spades, isFacedUp: true)
         let state = DraggingState(card: dragCard, position: CGPoint(x: 123, y: 123))
-        store.send(.dragCard(state.card, position: state.position)) {
+        await store.send(.dragCard(state.card, position: state.position)) {
             $0.draggingState = state
-            $0.zIndexPriority = .pile(id: 4)
+            $0.zIndexPriority = .pile(id: 7)
         }
-        store.send(.dropCards) {
+        await store.send(.dropCards) {
             $0.draggingState = nil
-            var pile4 = $0.piles[id: 4]!
-            pile4.cards.remove(dragCard)
 
-            var threeOfClubs = pile4.cards[id: Card(.three, of: .clubs, isFacedUp: false).id]!
-            threeOfClubs.isFacedUp = true
-            pile4.cards.updateOrAppend(threeOfClubs)
-            $0.piles.updateOrAppend(pile4)
+            $0.piles[id: 7]!.cards.remove(dragCard)
 
-            var pile5 = $0.piles[id: 5]!
-            pile5.cards.updateOrAppend(dragCard)
-            $0.piles.updateOrAppend(pile5)
+            $0.piles[id: 7]!.cards[id: Card(.seven, of: .hearts, isFacedUp: false).id]!.isFacedUp = true
+
+            $0.piles[id: 2]!.cards.updateOrAppend(dragCard)
         }
 
-        store.receive(.delegate(.scoringMove(.turnOverPileCard)))
+        await store.receive(.delegate(.scoringMove(.turnOverPileCard)))
 
-        scheduler.advance(by: 0.5)
+        await scheduler.advance(by: 0.5)
 
-        store.receive(.resetZIndexPriority) {
+        await store.receive(.resetZIndexPriority) {
             $0.zIndexPriority = .pile(id: 1)
         }
     }
 
-    func testDropCardsToAFoundation() {
+    func testDropCardsToAFoundation() async {
         let scheduler = DispatchQueue.test
-        store = TestStore(
+        let store = TestStore(
             initialState: Drag.State(
                 piles: GameCoreTests.pilesAfterShuffleForEasyGame(),
                 foundations: Game.State().foundations
@@ -102,18 +105,18 @@ class DragCoreTests: XCTestCase {
         }
 
         let frame: Frame = .foundation(Suit.spades.id, CGRect(x: 100, y: 100, width: 100, height: 200))
-        store.send(.updateFrames([frame])) {
+        await store.send(.updateFrames([frame])) {
             $0.frames = IdentifiedArrayOf(uniqueElements: [frame])
         }
 
         let dragCard = Card(.ace, of: .spades, isFacedUp: true)
         let state = DraggingState(card: dragCard, position: CGPoint(x: 123, y: 123))
-        store.send(.dragCard(state.card, position: state.position)) {
+        await store.send(.dragCard(state.card, position: state.position)) {
             $0.draggingState = state
             $0.zIndexPriority = .pile(id: 4)
         }
 
-        store.send(.dropCards) {
+        await store.send(.dropCards) {
             $0.draggingState = nil
             var pile4 = $0.piles[id: 4]!
             pile4.cards.remove(dragCard)
@@ -128,34 +131,43 @@ class DragCoreTests: XCTestCase {
             $0.foundations.updateOrAppend(spadesFoundation)
         }
 
-        store.receive(.delegate(.scoringMove(.moveToFoundation)))
+        await store.receive(.delegate(.scoringMove(.moveToFoundation)))
 
-        scheduler.advance(by: 0.5)
+        await scheduler.advance(by: 0.5)
 
-        store.receive(.resetZIndexPriority) {
+        await store.receive(.resetZIndexPriority) {
             $0.zIndexPriority = .pile(id: 1)
         }
     }
 
-    func testDragAndDropCardFromAFoundation() {
+    func testDragAndDropCardFromAFoundation() async {
         let scheduler = DispatchQueue.test
-        store.dependencies.mainQueue = scheduler.eraseToAnyScheduler()
-        testDropCardsToAFoundation()
+        let store = TestStore(
+            initialState: Drag.State(
+                piles: GameCoreTests.pilesAfterShuffleForEasyGame(),
+                foundations: Game.State().foundations
+            )
+        ) {
+            Drag()
+        } withDependencies: {
+            $0.mainQueue = scheduler.eraseToAnyScheduler()
+        }
+        await testDropCardsToAFoundation()
 
         let frame: Frame = .pile(5, CGRect(x: 300, y: 300, width: 100, height: 200))
         let frames = IdentifiedArrayOf(uniqueElements: store.state.frames + [frame])
-        store.send(.updateFrames(frames)) {
+        await store.send(.updateFrames(frames)) {
             $0.frames = frames
         }
 
         let dragCard = Card(.ace, of: .spades, isFacedUp: true)
         let state = DraggingState(card: dragCard, position: CGPoint(x: 323, y: 323))
-        store.send(.dragCard(state.card, position: state.position)) {
+        await store.send(.dragCard(state.card, position: state.position)) {
             $0.draggingState = state
             $0.zIndexPriority = .foundation(id: Suit.spades.id)
         }
 
-        store.send(.dropCards) {
+        await store.send(.dropCards) {
             $0.draggingState = nil
             var spadesFoundation = $0.foundations[id: Suit.spades.id]!
             spadesFoundation.cards.remove(dragCard)
@@ -166,30 +178,54 @@ class DragCoreTests: XCTestCase {
             $0.piles.updateOrAppend(pile5)
         }
 
-        store.receive(.delegate(.scoringMove(.moveBackFromFoundation)))
+        await store.receive(.delegate(.scoringMove(.moveBackFromFoundation)))
 
-        scheduler.advance(by: 0.5)
+        await scheduler.advance(by: 0.5)
 
-        store.receive(.resetZIndexPriority) {
+        await store.receive(.resetZIndexPriority) {
             $0.zIndexPriority = .pile(id: 1)
         }
     }
 
-    func testDoubleTapCardWithoutScoring() {
+    func testDoubleTapCardWithoutScoring() async {
+        let store = TestStore(
+            initialState: Drag.State(
+                piles: GameCoreTests.pilesAfterShuffle(),
+                foundations: Game.State().foundations
+            )
+        ) {
+            Drag()
+        }
         let card = Card(.six, of: .clubs, isFacedUp: true)
-        store.send(.doubleTapCard(card))
+        await store.send(.doubleTapCard(card))
     }
 
-    func testDoubleTapCardNotTheLastOneOfThePile() {
+    func testDoubleTapCardNotTheLastOneOfThePile() async {
         // TODO: write a test to check that double tapping on card that is not the last one of the pile
         // won't go to the fondation
+        let store = TestStore(
+            initialState: Drag.State(
+                piles: GameCoreTests.pilesAfterShuffle(),
+                foundations: Game.State().foundations
+            )
+        ) {
+            Drag()
+        }
         let card = Card(.six, of: .clubs, isFacedUp: true)
-        store.send(.doubleTapCard(card))
+        await store.send(.doubleTapCard(card))
     }
 
-    func testDoubleTapCardToScore() {
+    func testDoubleTapCardToScore() async {
+        let store = TestStore(
+            initialState: Drag.State(
+                piles: GameCoreTests.pilesAfterShuffle(),
+                foundations: Game.State().foundations
+            )
+        ) {
+            Drag()
+        }
         let card = Card(.ace, of: .clubs, isFacedUp: true)
-        store.send(.doubleTapCard(card)) {
+        await store.send(.doubleTapCard(card)) {
             $0.foundations[id: Suit.clubs.rawValue]?.cards.append(card)
             $0.piles[id: 1]?.cards.remove(card)
         }
